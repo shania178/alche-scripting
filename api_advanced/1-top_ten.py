19a4e1e0bf1a66a67217f78b241dc4e7372c18c8
shania178 <b.tinkouree@alustudent.com>
2026-08-22 17:57:18 +0000

    1. Top Ten

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Print the titles of first 10 hot posts."""
+    base_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "alche-api-advanced/1.0"
+    }
+
+    response = requests.get(
+        base_url,
+        headers=headers,
+        allow_redirects=False
+    )
+
+    if response.status_code != 200:
+        print(None)
+        return
+    try:
+        data = response.json()
+        posts = data["data"]["children"]
+
+        for post in posts[:10]:
+            print(post["data"]["title"])
+    except (KeyError, ValueError, TypeError):
+        print(None)
